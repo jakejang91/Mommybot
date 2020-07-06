75bec5cd18a0c915dd0f3df556a3d72928c38d3b
jakejang91 <32156141+jakejang91@users.noreply.github.com>
2020-07-06 12:03:32 +0900

    Update Arduino_LCDwithSerial.ino

--- Arduino_LCDwithSerial.ino
+++ Arduino_LCDwithSerial.ino
@@ -2,19 +2,19 @@
 //Compatible with the Arduino IDE 1.0
 //Library version:1.1
 #include <LiquidCrystal_I2C.h>
-// 아날로그 0, 1 5v gnd 에 연결
+// Connecting to  0, 1 5v gnd
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 int count = 0;
 int diod_pin = A1;
 void setup()
 {
-  lcd.init();                      // LCD 초기화
+  lcd.init();                      // LCD Initializing
   // Print a message to the LCD.
-  lcd.noBacklight();                // 백라이트 켜기
-  lcd.setCursor(0,0);             // 1번째, 1라인
+  lcd.noBacklight();                // Turn on Backlight
+  lcd.setCursor(0,0);             // First char, First line
   lcd.print("Hello");
-  lcd.setCursor(0,1);             // 1번째, 2라인
+  lcd.setCursor(0,1);             // First char, Second Line
   lcd.print("I'm Mommybot!");
   
   Serial.begin(9600);
@@ -28,17 +28,17 @@ void loop()
     //int outputValue = map(sensorValue, 0, 1023, 0, 255);
 
    // lcd.backlight();
-    while(!Serial){}
-    while(!Serial.available() ){}
-    String text = Serial.readString();
+    while(!Serial){} // wait unitil serial is working
+    while(!Serial.available() ){} // wait unitil serial is working
+    String text = Serial.readString(); // Get string from Serial port
 
     Serial.println(text);
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("   WARNING!");
     lcd.setCursor(0,1);
-    lcd.print(text);
-    for(int i = 0; i<100; i++){
+    lcd.print(text);            // print text
+    for(int i = 0; i<100; i++){ // Blink
       lcd.noBacklight();
       delay(50);
       lcd.backlight();
