0cc7e47c6453a0d26b821029b9106c9e8925a719
jakejang91 <32156141+jakejang91@users.noreply.github.com>
2020-07-06 14:51:03 +0900

    Update LogManager.py

--- t6modules/LogManager.py
+++ t6modules/LogManager.py
@@ -1,25 +1,27 @@
+# This module consists of defined functions to be used in the main code. 
+
 import time
 import csv
 import pandas as pd
 
 class LogManager():
-    def __init__(self):
+    def __init__(self): #to open CSV file.
         self.logs = pd.read_csv("logs.csv")
         print(self.logs.tail())
     
-    def getYesterdaySleep(self):
+    def getYesterdaySleep(self): #to record how many hours user slept last night.
         try:
             return self.logs.tail(1)['t2-01'].values[0]
         except:
             print("NONO YESTERDAY1")
     
-    def getLastdaysSleep(self):
+    def getLastdaysSleep(self): #to record how many hours on average user slept until today's date.
         try:
             return self.logs.tail(1)['t2-02'].values[0]
         except:
             print("NONO YESTERDAY2")
     
-    def saveLog(self, data):
+    def saveLog(self, data): # to record & save variables on opened csv file.
         try:
             
             with open('logs.csv', 'a') as f:
